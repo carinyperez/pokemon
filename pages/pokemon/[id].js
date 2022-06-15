ec8b61b36d8eca7d0ddd9d57e9fdcb8f68cf1982
Cariny <perezcariny@gmail.com>
2022-06-14 17:29:23 -0700

    Add client side rendering

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,37 @@
+import {useRouter} from 'next/router';
+import { useState, useEffect } from 'react';
+
+const Details = () => {
+    const {
+        query : {id}
+    } = useRouter();
+
+    const [pokemon, setPokemon] = useState([]); 
+    useEffect(() => {
+        const getPokemon = async () => {
+        const url = `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+        const response = await fetch(url);
+        setPokemon(await response.json());
+        }
+
+        if(id) {
+            getPokemon();
+        }
+
+    },[id]);
+    
+    if(!pokemon) {
+        return null 
+    }
+
+    return (
+        <div> 
+            <h3>{pokemon.name}</h3>
+            <img src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+            alt={pokemon.name}
+            />
+        </div>
+    )   
+};
+
+export default Details; 
